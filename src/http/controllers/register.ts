b981f62079e8824b65e57ebbed5212d43378c1fb
ismaelp96 <ismael6991@live.com>
2025-07-30 10:00:16 -0300

    feat: hash da senha e validação

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,37 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { hash } from "bcryptjs";
+
+import { prisma } from "@/lib/prisma";
+
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(6),
+  });
+
+  const { email, name, password } = registerBodySchema.parse(req.body);
+
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  });
+
+  return reply.status(201).send();
+}
