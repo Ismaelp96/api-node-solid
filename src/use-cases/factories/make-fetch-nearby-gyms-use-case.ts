cfc624fab006cadd1755988e2d69c75542434c1c
ismaelp96 <ismael6991@live.com>
2025-08-04 14:32:49 -0300

    feat: Factories dos casos de uso

---
+++ src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
@@ -0,0 +1,8 @@
+import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository";
+import { FeatchNearbyGymsUseCase } from "../fetch-nearby-gyms";
+
+export function makeFetchNearbyGymsUseCase() {
+  const gymsRepository = new PrismaGymsRepository();
+  const useCase = new FeatchNearbyGymsUseCase(gymsRepository);
+  return useCase;
+}
