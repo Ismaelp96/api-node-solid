a3d3966f40aa579399eb637e9119af722de7db34
ismaelp96 <ismael6991@live.com>
2025-08-05 11:24:04 -0300

    feat: outros controllers da academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,13 @@
-import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+
+import { search } from "./search";
+import { nearby } from "./nearby";
+import { create } from "./create";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+  app.post("/gyms", create);
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
 }
