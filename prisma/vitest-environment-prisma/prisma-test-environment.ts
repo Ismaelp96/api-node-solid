206cfca254732729705498ca18af7c9ffc6c030a
ismaelp96 <ismael6991@live.com>
2025-08-05 10:23:08 -0300

    feat: criando test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import type { Environment } from "vitest/environments";
+
+import { prisma } from "@/lib/prisma";
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL)
+    throw new Error("Plase provide a DATABASE_URL env variable");
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "ssr",
+  async setup() {
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+    console.log(`🧪 Criando DB com schema: ${schema}`);
+    process.env.DATABASE_URL = databaseUrl;
+    execSync("npx prisma db push");
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+        await prisma.$disconnect();
+        console.log(`🧹 Schema ${schema} apagado.`);
+      },
+    };
+  },
+};
